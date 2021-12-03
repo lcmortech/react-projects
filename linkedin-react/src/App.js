6be3adcf41935140963a00e3d47da1137e66824a
Lauren <rg326@live.com>
2021-12-03 17:30:49 -0500

    added list and logo

--- linkedin-react/src/App.js
+++ linkedin-react/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import logo from './logo.svg'
 
 function Header({name}){
   return(
@@ -8,9 +9,16 @@ function Header({name}){
   )
 }
 
-function Main({adj}){
+function Main({adj, dishes}){
   return(
-    <section>We make the most {adj} food around!</section>
+
+    <section>
+      <p>We make the most {adj} food around!</p>
+      <img src={logo} height={200} alt="the react logo"/>
+      <ul style={{ listStyle: "none"}}>
+      {dishes.map((dish, i) => <li key={i}>{dish}</li>)}
+      </ul>
+    </section>
   )
 }
 
@@ -22,11 +30,19 @@ function Footer({year}){
   )
 }
 
+const dishes = [
+  "Lamb Gyro",
+  "Spicy Tuna Roll",
+  "Bulgogi"
+];
+
+const dishObjects = dishes.map((dish, i)=> ({id: i, title: dish}))
+
 function App() {
   return (
     <div className="App">
       <Header name="Cindy"/>
-      <Main adj="amazing"/>
+      <Main adj="amazing" dishes={dishes}/>
       <Footer year={new Date().getFullYear()}/>
     </div>
   );

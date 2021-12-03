b548b36ce7bd2d9b0b72333f89a17380181ad69c
Lauren <rg326@live.com>
2021-12-02 21:51:54 -0500

    added components

---
+++ linkedin-react/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function Header(){
+  return(
+    <header>
+      <h1>Eve's Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(){
+  return(
+    <section>We make the best food around!</section>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ linkedin-react/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  React.createElement(
+    <ul>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+    </ul>
+    // "ul",
+    // null,
+    // React.createElement("li", null, "Monday"),
+    // React.createElement("li", null, "Tuesday"),
+    // React.createElement("li", null, "Wednesday")
+  ),
+    document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

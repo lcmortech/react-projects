95e130117b94f6ddcb4e512b6942a98ad49632b2
Lauren <rg326@live.com>
2021-12-08 17:27:24 -0500

    finalized files

--- linkedin-react/src/pages.js
+++ linkedin-react/src/pages.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, Outlet } from 'react-router-dom';
 
 export function Home() {
     return(
@@ -18,6 +18,7 @@ export function About() {
     return(
         <div>
             <h1>[About]</h1>
+            <Outlet />
         </div>
     )
 }
@@ -34,6 +35,30 @@ export function Contact() {
     return(
         <div>
             <h1>[Contact]</h1>
+        </div>
+    )
+}
+
+export function Services() {
+    return(
+        <div>
+            <h2>Our Services</h2>
+        </div>
+    )
+}
+
+export function CompanyHistory() {
+    return(
+        <div>
+            <h2>Our Company History</h2>
+        </div>
+    )
+}
+
+export function Location() {
+    return(
+        <div>
+            <h2>Our Location</h2>
         </div>
     )
 }

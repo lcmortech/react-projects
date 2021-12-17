9beae1b650f38a81ba4600f897e75cf19aa581c6
Lauren <rg326@live.com>
2021-12-16 23:26:54 -0500

    added react router

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './components/Home.js'
+import HomePage from './components/Home.js';
 
 function App() {
   return (

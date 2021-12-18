49313e3237a47ad6421baa71f2dc72aed60a9ebd
Lauren <rg326@live.com>
2021-12-17 21:23:10 -0500

    added routes to to route to fix routing problem

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage.js';
 import About from './components/About.js';
@@ -11,11 +11,13 @@ const App = () => {
     <Router>
       <div className="App">
         <NavBar />
-          <div class="page-body">
-            <Route path="/" component={HomePage} exact />
-            <Route path="/about" component={About} exact />
-            <Route path="/articles-list" component={ArticlesList} exact />
-            <Route path="/articles/:name" component={Articles} exact />
+          <div className="page-body">
+            <Routes>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/about" component={About} exact />
+              <Route path="/articles-list" component={ArticlesList} exact />
+              <Route path="/articles/:name" component={Articles} exact />
+            </Routes>
           </div>
       </div>
     </Router>

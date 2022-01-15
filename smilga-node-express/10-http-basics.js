fc896d5311dd78cd17653c981dc7cec576fdf5db
LC Morgan <rg326@live.com>
2022-01-14 19:44:29 -0500

    added conditional routes

--- smilga-node-express/10-http-basics.js
+++ smilga-node-express/10-http-basics.js
@@ -1,9 +1,30 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    const url = req.url
+    if (url === '/') {
     console.log('User hit the server')
+    console.log(req.method)
+    console.log(req.url)
     res.writeHead(200, {'content-type': 'text/html'})
-    res.end('<h1>home page</h1>')
+    res.write('<h1>home page</h1>')
+    res.end()
+    } 
+    //About page
+    else if (url === '/about') {
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    //404
+    res.writeHead(400, {content-type: 'text/html'})
+    res.write('<h1>Page not found, bro')
+    res.end()
+    }
 })
 
 server.listen(5000)
+
+//you ALWAYS need to call res.end() in order to end communication
+//yes, the content-type matters. 'text/html' will have the server read file as html, while
+//'text/plain' will read file as plain text

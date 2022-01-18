4b09cf740c11903e31168c8d424c85418eaa1209
LC Morgan <rg326@live.com>
2022-01-18 17:54:29 -0500

    set up a get req for api products

--- smilga-node-express/2-express-tutorial/19-express-api.js
+++ smilga-node-express/2-express-tutorial/19-express-api.js
@@ -3,7 +3,11 @@ const app = express()
 const { products } = require('./data')
 //GET
 app.get('/',(req, res) =>{
-    res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products',(req, res)=>{
+    
 })
 
 

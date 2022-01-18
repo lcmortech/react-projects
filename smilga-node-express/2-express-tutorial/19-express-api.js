628f4a921ceee60a93bac68385cfad507378d0e9
LC Morgan <rg326@live.com>
2022-01-18 17:55:44 -0500

    set up params for get requests

--- smilga-node-express/2-express-tutorial/19-express-api.js
+++ smilga-node-express/2-express-tutorial/19-express-api.js
@@ -6,8 +6,12 @@ app.get('/',(req, res) =>{
     res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
 })
 
-app.get('/api/products',(req, res)=>{
-    
+app.get('/api/products/1',(req, res)=>{
+    const newProducts = products.map(product => {
+        const { id,name,image } = product;
+        return { id,name,image }
+    })
+    res.json(newProducts)
 })
 
 

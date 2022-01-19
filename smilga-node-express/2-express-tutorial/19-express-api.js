775a77b9a7973984d69f38fdea97f23386bdccef
LC Morgan <rg326@live.com>
2022-01-19 17:06:11 -0500

    added alt empty data json for no products found result conditional

--- smilga-node-express/2-express-tutorial/19-express-api.js
+++ smilga-node-express/2-express-tutorial/19-express-api.js
@@ -48,7 +48,8 @@ app.get('/api/v1/query', (req, res)=>{
     }
 
     if(sortedProducts.length < 1){
-        res.status(200).send('No products matched your search')
+        //res.status(200).send('No products matched your search')
+        return res.status(200).json({success:true, data: []})
     }
 
     res.status(200).json(sortedProducts)

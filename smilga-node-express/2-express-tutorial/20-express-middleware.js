14742464be9ca1adc508c5a57dc10a2c4544bd0c
LC Morgan <rg326@live.com>
2022-01-20 18:39:47 -0500

    npm init folder, added middleware

--- smilga-node-express/2-express-tutorial/20-express-middleware.js
+++ smilga-node-express/2-express-tutorial/20-express-middleware.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
 
-//
+//req => middleware => response
+
+// app.get('/',(req, res)=>{
+//    const method = req.method
+//    const url = req.url
+//    const time = new Date().getFullYear
+//    console.log(method, url, time)
+//    res.send('Home')
+// })
+
+//middleware function
+const logger = (req,res,next) =>{
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear
+    console.log(method, url, time)
+    next()
+}
+
+app.get('/', logger, (req, res)=>{
+    res.send('Home')
+})
 
 
 app.listen(5000,()=>{

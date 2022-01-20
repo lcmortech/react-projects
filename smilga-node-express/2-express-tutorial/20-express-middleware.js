f162785f8f1cf9fd1e1d4ae975d1bd25580502bc
LC Morgan <rg326@live.com>
2022-01-20 18:39:47 -0500

    added notes for middleware and app.use

--- smilga-node-express/2-express-tutorial/20-express-middleware.js
+++ smilga-node-express/2-express-tutorial/20-express-middleware.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const logger = require('./logger')
 
 //req => middleware => response
+app.use(logger) 
+//auto invokes middleware for EVERY route
+//order matters, use must be at the top of the document, or else the get
+//requests will be invoked first
+//paths can be added: app.use('/api', logger)
+//applying the path applies to every route following the api
 
 // app.get('/',(req, res)=>{
 //    const method = req.method
@@ -12,16 +19,28 @@ const app = express()
 // })
 
 //middleware function
-const logger = (req,res,next) =>{
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear
-    console.log(method, url, time)
-    next()
-}
+//const logger = (req,res,next) =>{
+//    const method = req.method
+//    const url = req.url
+//    const time = new Date().getFullYear
+//    console.log(method, url, time)
+//    next()
+//}
 
 app.get('/', logger, (req, res)=>{
     res.send('Home')
+})
+
+app.get('/about', logger, (req, res)=>{
+    res.send('About')
+})
+
+app.get('/api/products',logger, (req, res)=>{
+    res.send('Products')
+})
+
+app.get('/api/items',logger,(req, res)=>{
+    res.send('Items')
 })
 
 

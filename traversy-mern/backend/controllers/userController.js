8fc17f2e337dc6d741bee86783c00c9c4266a234
Lauren <rg326@live.com>
2022-02-15 17:45:35 -0500

    added users model and user controller to mern project

---
+++ traversy-mern/backend/controllers/userController.js
@@ -0,0 +1,12 @@
+//@desc Register new user
+//@route POST /api/users
+//@access Public
+
+
+const registerUser = (req, res) => {
+    res.json({message: 'Register User'})
+}
+
+module.exports = {
+    registerUser,
+}

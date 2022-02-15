f11434e669e4a9eb9687a5eedbc69a2f0e7d080a
Lauren <rg326@live.com>
2022-02-14 21:09:45 -0500

    created restAPI for goals

--- traversy-mern/backend/controllers/goalController.js
+++ traversy-mern/backend/controllers/goalController.js
@@ -1,11 +1,15 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 // @desc Set goals
@@ -18,7 +22,11 @@ const setGoals = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Set goals'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 // @desc Update goals
@@ -26,7 +34,15 @@ const setGoals = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete goals
@@ -34,7 +50,18 @@ const updateGoals = asyncHandler(async (req, res) => {
 // @access Private
 
 const deleteGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+    res.status(200).json({id: req.params.id})
+    //const deleteGoal = await Goal.findByIdAndDelete(req.params.id, req.body)
+    //res.status(200).json(deleteGoal)
+
 })
 
 module.exports = {
--- traversy-mern/backend/server.js
+++ traversy-mern/backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
 const { errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+//Connect to Database
+connectDB()
 
 //ROUTES
 const app = express()
